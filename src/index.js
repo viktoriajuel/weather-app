73aaff6d6aa05d92c6cd31c42ff522096bf76078
viktoriajuel <viktoriajuel@outlook.com>
2023-03-12 19:04:43 +0100

    Changed to SheCodes weather API

--- src/index.js
+++ src/index.js
@@ -22,27 +22,24 @@ let currentTime = hours + ":" + minutes;
 let currentConditions = document.querySelector("#current-conditions");
 currentConditions.innerHTML = `Current conditions (${currentDay} ${currentTime})`;
 
+function showTemperature(response) {
+  let temperature = Math.round(response.data.temperature.current);
+  let temperatureElement = document.querySelector("#temp-element");
+  temperatureElement.innerHTML = `${temperature}°C`;
+}
+
 function updateCity(event) {
   event.preventDefault();
   let searchBar = document.querySelector("#search-bar");
   let city = searchBar.value;
   let cityHeading = document.querySelector(".chosen-city");
   cityHeading.innerHTML = `${city}`;
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiKey = "9a42a03oda7b4ctf1fd41c136ea3644b";
   let units = "metric";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`;
 
   axios.get(apiUrl).then(showTemperature);
 }
 
 let searchBar = document.querySelector("#search-form");
 searchBar.addEventListener("submit", updateCity);
-
-function showTemperature(response) {
-  let temperature = Math.round(response.data.main.temp);
-  console.log(temperature);
-  console.log(response);
-  let city = response.data.name;
-  let temperatureElement = document.querySelector("#temp-element");
-  temperatureElement.innerHTML = `${temperature}°C`;
-}
