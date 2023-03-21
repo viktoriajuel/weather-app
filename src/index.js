b22436e7c2a824195f8efae4b8f6561e2fb4a96b
viktoriajuel <viktoriajuel@outlook.com>
2023-03-21 11:18:59 +0100

    Adjusted forecast row (justify-content-between)

--- src/index.js
+++ src/index.js
@@ -32,8 +32,7 @@ function formatDay(timestamp) {
 function displayForecast(response) {
   let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
-  let forecastHTML = `<div class="row">`;
-
+  let forecastHTML = `<div class="row justify-content-between">`;
   forecast.forEach(function (forecastDay, index) {
     if (index < 5) {
       forecastHTML =
